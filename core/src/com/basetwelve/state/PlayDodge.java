bba0148cd456a254c0e1ec5abbe1970272644e94
Kenneth S <kksiu@wisc.edu>
2014-08-12 15:07:03 -0500

    Added Player Movement

--- core/src/com/basetwelve/state/PlayDodge.java
+++ core/src/com/basetwelve/state/PlayDodge.java
@@ -1,7 +1,11 @@
 package com.basetwelve.state;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 import com.badlogic.gdx.utils.Scaling;
 import com.basetwelve.entities.Dodger;
 import com.basetwelve.handlers.StateManager;
@@ -12,9 +16,20 @@ import com.basetwelve.handlers.StateManager;
 public class PlayDodge extends State {
     private Dodger playerDodge;
 
+    //movement keys
+    boolean moveLeftKey, moveRightKey, moveUpKey, moveDownKey;
+
+    //movement speed
+    private float movementSpeed = 100.0f;
 
     public PlayDodge(StateManager sm) {
         super(sm);
+
+        //movement keys
+        moveLeftKey = false;
+        moveRightKey = false;
+        moveUpKey = false;
+        moveDownKey = false;
 
         //make a texture region based off of a person
         TextureRegion texReg = new TextureRegion(game.getTextureHandler().getTexture("Player"), 17, 21);
@@ -22,7 +37,7 @@ public class PlayDodge extends State {
         //now make a dodger
         playerDodge = new Dodger(texReg);
 
-        float scale = 2.0f;
+        float scale = 3.0f;
 
         playerDodge.setWidth(scale * playerDodge.getWidth());
         playerDodge.setHeight(scale * playerDodge.getHeight());
@@ -31,7 +46,22 @@ public class PlayDodge extends State {
         //set in center
         playerDodge.setCenterPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
 
+        //add a stage listener for when the player moves
+        stage.addListener(new InputListener() {
+            @Override
+            public boolean keyDown(InputEvent event, int keycode) {
+                handleKeys(keycode, true);
+                return true;
+            }
 
+            @Override
+            public boolean keyUp(InputEvent event, int keycode) {
+                handleKeys(keycode, false);
+                return true;
+            }
+        });
+
+        Gdx.input.setInputProcessor(stage);
 
         //make the dodger show up on the screen
         stage.addActor(playerDodge);
@@ -43,6 +73,21 @@ public class PlayDodge extends State {
 
     @Override
     public void update(float dt) {
+        if(moveUpKey) {
+            playerDodge.addAction(parallel(moveBy(0, dt * movementSpeed)));
+        }
+
+        if(moveLeftKey) {
+            playerDodge.addAction(parallel(moveBy(-dt * movementSpeed, 0)));
+        }
+
+        if(moveDownKey) {
+            playerDodge.addAction(parallel(moveBy(0, -dt * movementSpeed)));
+        }
+
+        if(moveRightKey) {
+            playerDodge.addAction(parallel(moveBy(dt * movementSpeed, 0)));
+        }
 
         //have the stage act
         stage.act(dt);
@@ -56,5 +101,18 @@ public class PlayDodge extends State {
     @Override
     public void dispose() {
         stage.dispose();
+    }
+
+    //handles the key
+    public void handleKeys(int keycode, boolean pressDown) {
+        if(keycode == Input.Keys.W) {
+            moveUpKey = pressDown;
+        } else if(keycode == Input.Keys.A) {
+            moveLeftKey = pressDown;
+        } else if(keycode == Input.Keys.S) {
+            moveDownKey = pressDown;
+        } else if(keycode == Input.Keys.D) {
+            moveRightKey = pressDown;
+        }
     }
 }
