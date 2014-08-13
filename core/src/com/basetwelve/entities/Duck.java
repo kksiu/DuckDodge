f03bd0d381531b7ec289b7e7cd55abed92a8a38a
Kenneth S <kksiu@wisc.edu>
2014-08-13 00:26:42 -0500

    ducks now come in each at half a second

--- core/src/com/basetwelve/entities/Duck.java
+++ core/src/com/basetwelve/entities/Duck.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.Scaling;
 import com.basetwelve.MainClass;
+import com.basetwelve.state.State;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveTo;
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.parallel;
 
 /**
  * Created by Kenneth on 8/12/14.
@@ -17,11 +21,53 @@ public class Duck extends Image {
 
         //use texture region to create the duck actor
         scale = (Gdx.graphics.getWidth() / 15) / getWidth();
-        setWidth(0.5f * getWidth());
-        setHeight(0.5f * getHeight());
+        setWidth(scale * getWidth());
+        setHeight(scale * getHeight());
         setScaling(Scaling.fill);
 
         setOrigin((getWidth() / 2),
                 (getHeight() / 2));
+    }
+
+    //sends a duck at a position from one point to another point
+    public static Duck sendDuck(float startX, float startY, float endX, float endY, float speed, MainClass game) {
+        Duck duck = new Duck(game);
+
+        //start the duck at the original location
+        if(startX <= 0) {
+            duck.setX(-duck.getWidth());
+            duck.setY(startY);
+        } else if(startX >= Gdx.graphics.getWidth()) {
+            duck.setX(Gdx.graphics.getWidth());
+            duck.setY(startY);
+        } else if(startY <= 0) {
+            duck.setX(startX);
+            duck.setY(-duck.getHeight());
+        } else if(startY >= Gdx.graphics.getHeight()) {
+            duck.setX(startX);
+            duck.setY(Gdx.graphics.getHeight());
+        } else {
+            duck.setX(startX);
+            duck.setY(startY);
+        }
+
+        //end location
+        if(endX <= 0) {
+            endX = -duck.getWidth();
+        } else if(endX >= Gdx.graphics.getWidth()) {
+            endX = Gdx.graphics.getWidth();
+        } else if(endY <= 0) {
+            endY = -duck.getHeight();
+        } else if(endY >= Gdx.graphics.getHeight()) {
+            endY = Gdx.graphics.getHeight();
+        }
+
+        //get the distance of the movement
+        float distance = (float) Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2));
+
+        //now add a move to function with the speed
+        duck.addAction(parallel(moveTo(endX, endY, distance / speed)));
+
+        return duck;
     }
 }
--- core/src/com/basetwelve/state/PlayDodge.java
+++ core/src/com/basetwelve/state/PlayDodge.java
@@ -7,11 +7,19 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 
+import com.badlogic.gdx.scenes.scene2d.actions.RepeatAction;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Scaling;
 import com.basetwelve.entities.CircleControl;
 import com.basetwelve.entities.Dodger;
+import com.basetwelve.entities.Duck;
 import com.basetwelve.handlers.StateManager;
+import com.badlogic.gdx.utils.Timer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 /**
  * Created by Kenneth on 8/11/14.
@@ -36,8 +44,24 @@ public class PlayDodge extends State {
     //movement speed
     private float movementSpeed = Gdx.graphics.getWidth() / 5;
 
+    //list of ducks
+    private List<Duck> duckList;
+
+    //random object for duck entrances
+    private Random rand;
+    private Timer duckTimer;
+    private int duckCount;
+    private float duration = 0.5f;
+    private Timer.Task duckTask;
+
     public PlayDodge(StateManager sm) {
         super(sm);
+
+        //initialize random
+        rand = new Random();
+
+        //list of ducks
+        duckList = new ArrayList<Duck>();
 
         //movement keys
         moveLeftKey = false;
@@ -117,6 +141,17 @@ public class PlayDodge extends State {
         //make the dodger show up on the screen
         stage.addActor(playerDodge);
         stage.addActor(controlMove);
+
+        duckTask = new Timer.Task() {
+            @Override
+            public void run() {
+                sendPrivateduck();
+            }
+        };
+
+        //add random ducks
+        duckTimer = new Timer();
+        duckTimer.scheduleTask(duckTask, 0.0f, duration);
     }
 
     @Override
@@ -151,6 +186,28 @@ public class PlayDodge extends State {
 
             if(moveRightKey) {
                 playerDodge.addAction(parallel(moveBy(dt * movementSpeed, 0)));
+            }
+        }
+
+        //get all the duck lists
+        for(int i = 0; i < duckList.size(); i++) {
+            Duck duck = duckList.get(i);
+            //if all actions are done (i.e., off the screen), remove
+            if(duck.getActions().size == 1) {
+                duckList.remove(duck);
+                duck.remove();
+            }
+        }
+
+        //check the duck and see if it is time to make it faster
+        if(duckCount == 5) {
+            if(duration >= 1.0f) {
+                duration = duration - 0.5f;
+                duckCount = 0;
+                duckTask.cancel();
+                duckTimer.stop();
+                duckTimer.scheduleTask(duckTask, 0.0f, duration);
+                duckTimer.start();
             }
         }
 
@@ -191,5 +248,80 @@ public class PlayDodge extends State {
         Math.pow(event.getStageX() - (control.getX()  + (control.getWidth() / 2)), 2));
     }
 
-    
+    private void sendPrivateduck() {
+        float startX = 0f;
+        float startY = 0f;
+        float endX = 0f;
+        float endY = 0f;
+
+        int sign = rand.nextBoolean() ? 1 : -1;
+
+        //check if start from width or height
+        boolean startWidth = rand.nextBoolean();
+        if(startWidth) {
+            startX = Gdx.graphics.getWidth();
+            startY = rand.nextFloat() * Gdx.graphics.getHeight();
+        } else {
+            startX = rand.nextFloat() * Gdx.graphics.getWidth();
+            startY = Gdx.graphics.getHeight();
+        }
+
+        //now determine the end points (make sure that it is not the opposite
+        int endSide = rand.nextInt(5);
+        if(startWidth) {
+            switch(endSide) {
+                //top side
+                case 0:
+                    endX = rand.nextFloat() * Gdx.graphics.getWidth();
+                    endY = Gdx.graphics.getHeight();
+                    break;
+
+                //bottom side
+                case 1:
+                    endX = rand.nextFloat() * Gdx.graphics.getWidth();
+                    endY = 0;
+                    break;
+
+                //opposite
+                default:
+                    endX = -1 * startX;
+                    endY = rand.nextFloat() * Gdx.graphics.getHeight();
+                    break;
+            }
+
+        } else {
+            switch(endSide) {
+                //left side
+                case 0:
+                    endX = 0;
+                    endY = rand.nextFloat() * Gdx.graphics.getHeight();
+                    break;
+
+                //right side
+                case 1:
+                    endX = Gdx.graphics.getWidth();
+                    endY = rand.nextFloat() * Gdx.graphics.getHeight();
+                    break;
+
+                //do the opposite side
+                default:
+                    endX = rand.nextFloat() * Gdx.graphics.getWidth();
+                    endY = -1 * startY;
+                    break;
+            }
+
+        }
+
+        Duck duck = Duck.sendDuck(startX, startY, endX, endY, 100f, game);
+        RepeatAction repeatAction = new RepeatAction();
+        repeatAction.setAction(parallel(rotateBy(360, 1.0f)));
+        repeatAction.setCount(RepeatAction.FOREVER);
+        duck.addAction(repeatAction);
+
+        duckList.add(duck);
+        stage.addActor(duck);
+        duck.toBack();
+
+        duckCount++;
+    }
 }
